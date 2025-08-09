14a5a51ef4f79c3cb4e3f94e6aaebf13ca206130
Adamskiee <tosinomatthew@gmail.com>
2025-08-09 17:54:07 +0800

    Add initial project setup with Tailwind CSS configuration, input styles, and package.json
    - Created package.json to manage project dependencies and scripts.
    - Added input.css for Tailwind CSS styles including custom properties and font faces.
    - Configured tailwind.config.js to extend theme with custom colors and breakpoints.

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      screens: {
+        desktop: "1139px"
+      },
+      colors: {
+        "red": "var(--red)",
+        "blue-800": "var(--blue-800)",
+        "blue-700": "var(--blue-700)",
+        "grey": "var(--grey)",
+        "white": "var(--white)"
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
