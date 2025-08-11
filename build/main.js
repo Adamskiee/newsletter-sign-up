fe9fe6268eb44a44749e723665734a2e09ad1655
Adamskiee <tosinomatthew@gmail.com>
2025-08-12 07:22:55 +0800

    Fix email input ID and validation handling; update font path in CSS

--- build/main.js
+++ build/main.js
@@ -6,7 +6,7 @@ const card = document.getElementById("card");
 const emailSubmit = document.getElementById("email-text");
 
 const handleSubmit = (e) => {
-    e.preventDefault(e);
+    e.preventDefault();
     card.classList.toggle("hidden");
     successMsg.classList.toggle("hidden");
     emailSubmit.textContent = emailInput.value;
@@ -16,10 +16,12 @@ form.addEventListener("submit", handleSubmit);
 
 emailInput.addEventListener("invalid", (e)=>{
     let inputValue = e.target;
-    if(inputValue.validity.typeMismatch || inputValue.validity.typeMismatch) {
+    if(inputValue.validity.typeMismatch || inputValue.validity.valueMissing) {
         inputValue.setCustomValidity(" ");
         errorMsg.classList.remove("hidden");
         emailInput.classList.add("input-error");
+    }else {
+        handleSubmit(e);
     }
 })
 emailInput.addEventListener("input", (e)=> {
